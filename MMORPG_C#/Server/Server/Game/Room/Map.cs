70a9f0cc6bf8eea02df65192f1c21ec8c1e0a22f
PARKGIBAEK <tkdring@naver.com>
2021-05-30 00:51:15 +0900

    Added comments for A* Pathfinding

--- MMORPG_C#/Server/Server/Game/Room/Map.cs
+++ MMORPG_C#/Server/Server/Game/Room/Map.cs
@@ -49,9 +49,16 @@ namespace Server.Game
 
 		public int CompareTo(PQNode other)
 		{
-			if (F == other.F)
+			if (F < other.F)
+				return 1;
+			else if (F > other.F)
+				return -1;
+			else
 				return 0;
-			return F < other.F ? 1 : -1;
+			
+			//if (F == other.F)
+			//	return 0;
+			//return F < other.F ? 1 : -1;
 		}
 	}
 
@@ -249,100 +256,114 @@ namespace Server.Game
 		int[] _deltaX = new int[] { 0, 0, -1, 1 };
 		int[] _cost = new int[] { 10, 10, 10, 10 };
 
-		public List<Vector2Int> FindPath(Vector2Int startCellPos, Vector2Int destCellPos, bool checkObjects = true, int maxDist = 10)
+	public List<Vector2Int> FindPath(Vector2Int startCellPos, Vector2Int destCellPos, bool checkObjects = true, int maxDist = 10)
+	{
+		List<Pos> path = new List<Pos>();
+
+		// 점수 매기기
+		// F = G + H
+		// F = 최종 점수 (작을 수록 좋음, 경로에 따라 달라짐)
+		// G = 시작점에서 해당 좌표까지 이동하는데 드는 비용 (작을 수록 좋음, 경로에 따라 달라짐)
+		// H = 목적지에서 얼마나 가까운지 (작을 수록 좋음, 고정)
+
+		// (y, x)에 대한 방문 여부를 확인하기 위함 (closedList에 존재하는 노드 = 방문했던 적이 있음을 의미)
+		HashSet<Pos> closedList = new HashSet<Pos>(); // 노드의 방문 여부를 기록
+
+		// (y, x) 가는 길을 한 번이라도 발견했는지
+		// 발견X => MaxValue
+		// 발견O => F = G + H
+
+		// 노드에 대한 F Cost를 평가하여 기록하는 용도
+		// 만약 과거에 평가된 노드일지라도 새로 평가된 F Cost가 더 작다면 F Cost를 갱신한다 ( 경로를 기록하는 parent도 함께 갱신 )
+		Dictionary<Pos, int> openedList = new Dictionary<Pos, int>(); // currentNode를 기준으로 탐색한 neighborNode를 저장
+		//최적의 경로를 기록하는 용도
+		//예를들면 (3,1) -> (3,2) -> (2,2)가 최적의 경로일 경우
+		//parent[(2,2)] = (3,2);
+		//parent[(3,2)] = (3,1);
+		Dictionary<Pos, Pos> parent = new Dictionary<Pos, Pos>();//value 노드는 key 노드로 이동하기 직전 경로(위치)를 의미
+
+		// 오픈리스트에 있는 정보들 중에서, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
+		PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();//우선 순위는 F Cost가 낮은 것부터
+
+		// CellPos -> ArrayPos
+		Pos pos = Cell2Pos(startCellPos);//처음에는 일단 시작노드의 위치를 받아온다
+		Pos destination = Cell2Pos(destCellPos);
+
+		// 시작점 입력 (예약 진행)
+		openedList.Add(pos, 10 * (Math.Abs(destination.Y - pos.Y) + Math.Abs(destination.X - pos.X)));
+
+		pq.Push(new PQNode() { 
+			F = 10 * (Math.Abs(destination.Y - pos.Y) + Math.Abs(destination.X - pos.X)),
+			G = 0, Y = pos.Y, X = pos.X }
+		);
+		parent.Add(pos, pos);//처음 시작 위치이므로 동일 노드끼리 연결
+
+		while (pq.Count > 0)
 		{
-			List<Pos> path = new List<Pos>();
+			// 제일 좋은 후보를 찾는다
+			PQNode pqNode = pq.Pop();
+			Pos current = new Pos(pqNode.Y, pqNode.X);
 
-			// 점수 매기기
-			// F = G + H
-			// F = 최종 점수 (작을 수록 좋음, 경로에 따라 달라짐)
-			// G = 시작점에서 해당 좌표까지 이동하는데 드는 비용 (작을 수록 좋음, 경로에 따라 달라짐)
-			// H = 목적지에서 얼마나 가까운지 (작을 수록 좋음, 고정)
+			// 이미 방문한 노드일 경우 스킵
+			if (closedList.Contains(current))
+				continue;
 
-			// (y, x) 이미 방문했는지 여부 (방문 = closed 상태)
-			HashSet<Pos> closeList = new HashSet<Pos>(); // CloseList
+			// 방문한 노드 기록
+			closedList.Add(current);
 
-			// (y, x) 가는 길을 한 번이라도 발견했는지
-			// 발견X => MaxValue
-			// 발견O => F = G + H
-			Dictionary<Pos, int> openList = new Dictionary<Pos, int>(); // OpenList
-			Dictionary<Pos, Pos> parent = new Dictionary<Pos, Pos>();
+			// 목적지에 도착했으면 탐색 종료
+			if (current.Y == destination.Y && current.X == destination.X)
+				break;
 
-			// 오픈리스트에 있는 정보들 중에서, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
-			PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
-
-			// CellPos -> ArrayPos
-			Pos pos = Cell2Pos(startCellPos);
-			Pos dest = Cell2Pos(destCellPos);
-
-			// 시작점 발견 (예약 진행)
-			openList.Add(pos, 10 * (Math.Abs(dest.Y - pos.Y) + Math.Abs(dest.X - pos.X)));
-
-			pq.Push(new PQNode() { F = 10 * (Math.Abs(dest.Y - pos.Y) + Math.Abs(dest.X - pos.X)), G = 0, Y = pos.Y, X = pos.X });
-			parent.Add(pos, pos);
-
-			while (pq.Count > 0)
+			//인근 노드 탐색 시작
+			// (상하좌우)이동할 수 있는 좌표인지 확인해서 예약(open)한다
+			for (int i = 0; i < _deltaY.Length; i++)
 			{
-				// 제일 좋은 후보를 찾는다
-				PQNode pqNode = pq.Pop();
-				Pos node = new Pos(pqNode.Y, pqNode.X);
-				// 동일한 좌표를 여러 경로로 찾아서, 더 빠른 경로로 인해서 이미 방문(closed)된 경우 스킵
-				if (closeList.Contains(node))
+				Pos neighbor = new Pos(current.Y + _deltaY[i], current.X + _deltaX[i]);// i값에 따라 상/하/좌/우 탐색
+
+				// 최대 이동거리를 초과할 경우 탐색 스킵
+				if (Math.Abs(pos.Y - neighbor.Y) + Math.Abs(pos.X - neighbor.X) > maxDist)
 					continue;
 
-				// 방문한다
-				closeList.Add(node);
-
-				// 목적지 도착했으면 바로 종료
-				if (node.Y == dest.Y && node.X == dest.X)
-					break;
-
-				// 상하좌우 등 이동할 수 있는 좌표인지 확인해서 예약(open)한다
-				for (int i = 0; i < _deltaY.Length; i++)
-				{
-					Pos next = new Pos(node.Y + _deltaY[i], node.X + _deltaX[i]);
-
-					// 너무 멀면 스킵
-					if (Math.Abs(pos.Y - next.Y) + Math.Abs(pos.X - next.X) > maxDist)
+				// 유효 범위를 벗어났으면 스킵
+				if (neighbor.Y != destination.Y || neighbor.X != destination.X)
+				{	// 벽으로 막혀서 갈 수 없으면 스킵
+					if (CanGo(Pos2Cell(neighbor), checkObjects) == false) // CellPos
 						continue;
-
-					// 유효 범위를 벗어났으면 스킵
-					// 벽으로 막혀서 갈 수 없으면 스킵
-					if (next.Y != dest.Y || next.X != dest.X)
-					{
-						if (CanGo(Pos2Cell(next), checkObjects) == false) // CellPos
-							continue;
-					}
-
-					// 이미 방문한 곳이면 스킵
-					if (closeList.Contains(next))
-						continue;
-
-					// 비용 계산
-					int g = 0;// node.G + _cost[i];
-					int h = 10 * ((dest.Y - next.Y) * (dest.Y - next.Y) + (dest.X - next.X) * (dest.X - next.X));
-					// 다른 경로에서 더 빠른 길 이미 찾았으면 스킵
-
-					int value = 0;
-					if (openList.TryGetValue(next, out value) == false)
-						value = Int32.MaxValue;
-
-					if (value < g + h)
-						continue;
-
-					// 예약 진행
-					if (openList.TryAdd(next, g + h) == false)
-						openList[next] = g + h;
-
-					pq.Push(new PQNode() { F = g + h, G = g, Y = next.Y, X = next.X });
-
-					if (parent.TryAdd(next, node) == false)
-						parent[next] = node;
 				}
-			}
 
-			return CalcCellPathFromParent(parent, dest);
+				// 이미 방문한 곳이면 스킵
+				if (closedList.Contains(neighbor))//※시작점으로 부터 우선순위가 높은 것부터 BFS방식으로 탐색하기 때문에 재방문은 스킵해도 됨
+					continue;
+
+				// 현재 경로를 기준으로 현재 노드 평가
+				// G Cost : distance from starting node(실제로 평가 된 값)
+				// H Cost : distance from target node(추정된 값)
+				// F Cost = G Cost + H Cost
+				int g = 0;//  pqNode.G + _cost[i]; 맵이 복잡할 경우 사용
+				int h = 10 * ((destination.Y - neighbor.Y) * (destination.Y - neighbor.Y) + (destination.X - neighbor.X) * (destination.X - neighbor.X));
+					
+				int value;
+				// NeighborNode에 대한 평가 기록이 있는지 확인
+				if (openedList.TryGetValue(neighbor, out value) == false)//TryGetValue 매서드는 value를 복사하여 반환한다.
+					value = Int32.MaxValue;//평가 기록이 없을 경우 다른 경로에서 한번도 방문한 적이 없다는 의미
+				// 기존에 탐색된 경로가 더 빠를 경우 스킵
+				if (value < g + h)
+					continue;
+
+				// 평가 기록 예약 진행
+				if (openedList.TryAdd(neighbor, g + h) == false)//TryAdd 매서드는 key가 이미 존재할 경우 아무 동작을 하지 않는다
+					openedList[neighbor] = g + h;//추가 실패할 경우 key가 이미 존재한다는 것이므로, F Cost만 변경해준다
+
+				pq.Push(new PQNode() { F = g + h, G = g, Y = neighbor.Y, X = neighbor.X });
+
+				if (parent.TryAdd(neighbor, current) == false)
+					parent[neighbor] = current;
+			}
 		}
+
+		return CalcCellPathFromParent(parent, destination);
+	}
 
 		List<Vector2Int> CalcCellPathFromParent(Dictionary<Pos, Pos> parent, Pos dest)
 		{
@@ -393,7 +414,7 @@ namespace Server.Game
 			return new Vector2Int(pos.X + MinX, MaxY - pos.Y);
 		}
 
-		#endregion
+		#endregion A* PathFinding
 	}
 
 }
